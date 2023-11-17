45bee2d396c73a91ded5be58b6246cc4e70e8e90
Sabrinavf <contactsabrinafernandes@gmail.com>
2023-11-17 18:39:14 -0300

    Adicionar arquivos de projeto.

---
+++ somaSimplesEmCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Estudo
+{
+    class somaSimples
+    {
+        static void Main(string[] args)
+        {
+            int SOMA;
+
+            string[] vet = (Console.ReadLine().Split(" "));
+            int A = int.Parse(vet[0]);
+            int B = int.Parse(vet[1]);
+
+            SOMA = A + B;
+
+            Console.WriteLine("SOMA = " + SOMA);
+
+
+
+        }
+    }
+}
